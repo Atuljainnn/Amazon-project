5e3143d4e94d969488a26baa63ab0315a9279bea
atuljainnn <mratul3877@gmail.com>
2024-05-03 00:30:56 +0530

    move addToCart into cart.js and use functions

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,23 @@
 export const cart = [];
+
+export function addToCart(productId){
+
+  let matchingItem=0;
+
+  
+  cart.forEach((cartItem)=>{
+   if(productId===cartItem.productId){
+    matchingItem=cartItem;
+   }
+   
+  });
+
+  if(matchingItem){
+    matchingItem.quantity+=1;
+  }else
+    cart.push({
+      productId:productId,
+      quantity:1
+    });
+
+   }
