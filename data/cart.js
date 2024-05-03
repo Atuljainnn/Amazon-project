5f7b9606ddc139ac5634a48b0aaefca0473fa696
atuljainnn <mratul3877@gmail.com>
2024-05-03 18:48:45 +0530

    savvve cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [
+export let cart =JSON.parse(localStorage.getItem('cart')) || [
   {
    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity:2
@@ -8,6 +8,9 @@ export let cart = [
   }
 ];
 
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addToCart(productId){
 
   let matchingItem=0;
@@ -27,6 +30,7 @@ export function addToCart(productId){
       productId:productId,
       quantity:1
     });
+    saveToStorage();
 
    }
 
@@ -40,5 +44,6 @@ export function addToCart(productId){
       }
     });
     cart=newCart;
+    saveToStorage();
     
    }
