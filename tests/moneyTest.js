0ea247ecf058c61fd7d54fe03a08d8c618e0fddb
atuljainnn <mratul3877@gmail.com>
2024-05-14 12:20:22 +0530

    organize code

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -2,6 +2,7 @@ import {formatCurrency} from "../scripts/utils/money.js";
 
 console.log('test suite:formatCurrency');
 
+
 console.log('convert cents into dollars');
 
 if(formatCurrency(2095)==='20.95'){
@@ -9,6 +10,7 @@ if(formatCurrency(2095)==='20.95'){
 }else{
   console.log('failed');
 }
+
 
 console.log('works with 0');
 
