1ab5245726113409d4d82711dc09102153cf3a13
atuljainnn <mratul3877@gmail.com>
2024-05-18 19:51:17 +0530

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checout/orderSummary.js";
 import { renderPaymentSummary } from "./checout/paymentSummary.js";
+import '../data/cart-oop..js';
 
 renderOrderSummary();
 renderPaymentSummary();
