2fe1a6b7b35448d3beed9907516be5c531428d49
atuljainnn <mratul3877@gmail.com>
2024-05-23 01:17:47 +0530

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import { renderOrderSummary } from "./checout/orderSummary.js";
 import { renderPaymentSummary } from "./checout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js';
-import '../data/backend-practice.js'
+//import '../data/backend-practice.js'
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(()=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+});
