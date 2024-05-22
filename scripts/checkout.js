9479a4fc6f382510c877be184cdbad362e90c0c7
atuljainnn <mratul3877@gmail.com>
2024-05-22 23:57:46 +0530

    sends a http request

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checout/orderSummary.js";
 import { renderPaymentSummary } from "./checout/paymentSummary.js";
 //import '../data/cart-class.js';
+import '../data/backend-practice.js'
 
 renderOrderSummary();
 renderPaymentSummary();
