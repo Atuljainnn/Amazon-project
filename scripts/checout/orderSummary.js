a2c7c4fa88cf2da0b7aa14b5844355ac71882da5
atuljainnn <mratul3877@gmail.com>
2024-08-01 22:51:30 +0530

    make items quantity interactive in payments details

--- scripts/checout/orderSummary.js
+++ scripts/checout/orderSummary.js
@@ -116,8 +116,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
     link.addEventListener('click',()=>{
       const productId= link.dataset.productId;
       removeFromCart(productId);
-      const container= document.querySelector(`.js-cart-item-container-${productId}`);
-      container.remove();
+      
+      renderOrderSummary();
       renderPaymentSummary();
       updateCartQuantity();
     });
@@ -168,6 +168,7 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
       );
       quantityLabel.innerHTML = newQuantity;
       updateCartQuantity();
+      renderPaymentSummary();
 
     });
   });
